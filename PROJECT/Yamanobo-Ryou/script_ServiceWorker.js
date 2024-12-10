924d649bce2a3b5ab4e0082dfe1e1755c6828bb2
candy10243 <kingstone020506@gmail.com>
2024-12-11 02:23:12 +0800

    v1.16 Introduce tooltip.

--- PROJECT/Yamanobo-Ryou/script_ServiceWorker.js
+++ PROJECT/Yamanobo-Ryou/script_ServiceWorker.js
@@ -6,7 +6,7 @@
 	// Declare variables
 	"use strict";
 		// Unsaved
-		const CacheName = "Yamanobo-Ryou_v1.15";
+		const CacheName = "Yamanobo-Ryou_v1.16";
 
 // Listeners
 	// Service worker (https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/#step-5---add-a-service-worker)

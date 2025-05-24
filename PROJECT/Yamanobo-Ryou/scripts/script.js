bdc6f158f251683be254e7e1540ce16eebcdd434
candy10243 <kingstone020506@gmail.com>
2025-05-25 01:02:34 +0800

    Following home page update.

--- PROJECT/Yamanobo-Ryou/scripts/script.js
+++ PROJECT/Yamanobo-Ryou/scripts/script.js
@@ -137,7 +137,7 @@
 		}
 		switch(System.I18n.Language) {
 			case "Auto":
-				// navigator.language ...
+				// navigator.languages ...
 				break;
 			case "en-US":
 				/* ChangeCursorOverall("wait");
